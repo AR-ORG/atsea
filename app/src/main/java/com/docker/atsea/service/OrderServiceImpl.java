bc47050ee0898c0997263b45120f5c40909b92c5
Guillaume Morini <guillaume.morini@docker.com>
2019-01-31 09:50:11 -0600

    fix findOne error due to version upgrade

--- app/src/main/java/com/docker/atsea/service/OrderServiceImpl.java
+++ app/src/main/java/com/docker/atsea/service/OrderServiceImpl.java
@@ -18,7 +18,7 @@ public class OrderServiceImpl implements OrderService {
 	private OrderRepository orderRepository;
 		
 	public Order findById(Long orderId) {
-		return orderRepository.findOne(orderId) ;
+		return orderRepository.getOne(orderId) ;
 	}
 
 	public Order createOrder(Order order) {		
